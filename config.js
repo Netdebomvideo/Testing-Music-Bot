815c4361be0e167f48fcd7892f79272ece4a6311
Netdebom video <debomvideo@gmail.com>
2025-03-08 00:25:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+    TOKEN: "",
+    language: "en",
+    ownerID: ["1344736094561894460", ""], 
+    mongodbUri : "mongodb+srv://debomvideo:Debom@2727@discordbot.xjlkr.mongodb.net/?retryWrites=true&w=majority",
+    spotifyClientId : "c8211e8c9aa84e74a421c36a8ce87bd1",
+    spotifyClientSecret : "8a444402cb8f4f5b8adff5dbaf367614",
+    setupFilePath: './commands/setup.json',
+    commandsDir: './commands',  
+    embedColor: "#1db954",
+    activityName: "DJ Music", 
+    activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+    SupportServer: "https://discord.gg/xQF9f9yUEM",
+    embedTimeout: 5, 
+    errorLog: "", 
+    nodes: [
+       {
+        name: "XX",
+        password: "enteryourcustompass",
+        host: "180.188.226.76",
+        port:  7019,
+        secure: false
+      }
+    ]
+  }
